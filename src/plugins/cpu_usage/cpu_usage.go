8cba4e0be121cbbd310cedebdf514e0bfe4bec19
Erik Hollensbe <erik+github@hollensbe.org>
2013-09-07 20:46:47 -0700

    remove debugging; add comments to cpu_usage monitor

--- src/plugins/cpu_usage/cpu_usage.go
+++ src/plugins/cpu_usage/cpu_usage.go
@@ -27,15 +27,20 @@ func get_jiffies() (jiffies int64, cpus int64) {
 
 	for _, line := range lines {
 		if strings.Index(line, "cpu ") == 0 {
+			/* cpu with no number is the aggregate of all of them -- this is what we
+			 * want to parse
+			 */
 			parts := strings.Split(line, " ")
-			fmt.Println(parts)
 
+			/* 2 - 11 are the time aggregates */
 			for x := 2; x <= 11; x++ {
 
+				/* 5 is the idle time, which we don't want */
 				if x == 5 {
 					continue
 				}
 
+				/* integer all the things */
 				part, err := strconv.Atoi(parts[x])
 
 				if err != nil {
@@ -44,7 +49,11 @@ func get_jiffies() (jiffies int64, cpus int64) {
 
 				jiffies += int64(part)
 			}
+
 		} else if strings.Index(line, "cpu") == 0 {
+			/* cpu with a number is the specific time -- cheat and use this for the
+			 * processor count since we've already read it
+			 */
 			cpus++
 		}
 	}
@@ -62,6 +71,5 @@ func get_jiffy_diff() (int64, int64) {
 
 func GetMetric(params interface{}) interface{} {
 	diff, cpus := get_jiffy_diff()
-	fmt.Println(diff, C.get_hz())
 	return [2]float64{float64(cpus), (float64(diff) / float64(C.get_hz()))}
 }
